3d0648b08af1ec17ae31fd58478ded2a20a95da4
osd <info@osd84.fr>
2025-06-16 16:46:24 +0200

    + Tests Ok

--- tests/FmtTest.php
+++ tests/FmtTest.php
@@ -27,10 +27,4 @@ $tester->assertEqual('No', $resultEmptyString, "bool retourne 'No' pour une chaÃ
 $resultNonEmptyString = Fmt::bool('hello');
 $tester->assertEqual('Yes', $resultNonEmptyString, "bool retourne 'Yes' pour une chaÃ®ne non vide");
 
-
-// Test de dateTimeOrNull
-$tester->header("Test de dateTimeOrNull()");
-$dateTime = Fmt::dateTimeOrNull('2021-01-01 12:00:00');
-$tester->assertEqual($dateTime, '2021-01-01 12:00:00', "dateTimeOrNull retourne la date et l'heure");
-
 $tester->footer(exit: false);
